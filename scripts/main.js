97e0c759e0a8e512dd46d232af06f03d2b56b8ff
tomeir <tsmeland@gmail.com>
2019-05-08 12:26:12 +0200

    Synkroniserte med laptop

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 var myImage = document.querySelector('img');
 
+/*
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/baking-time.jpg') {
@@ -8,23 +9,25 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/baking-time.jpg');
     }
 }
-
+*/
 var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
+//var myHeading = document.querySelector('h1');
+/*
 function setUserName() {
     var myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'So you wanna bake, huh ' + myName + '?';
+    myHeading.innerHTML = 'Welcome to Pure AU ' + myName;
 }
+
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'So you wanna bake, huh ' + storedName + '?';
+  myHeading.innerHTML = 'Welcome to Pure Au ' + storedName;
 }
+*/
 
-myButton.onclick = function() {
-  setUserName();
-}
+//myButton.onclick = function() {
+//  setUserName();
+//}
